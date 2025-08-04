5d3e9e49794e331d00da57f3800d566ec4cc83d2
GATkassTACA <kmounce82@gmail.com>
2025-08-04 08:37:08 -0500

    gg

---
+++ index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+print("Content-Type: text/html\n")
+print("""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PharmAssist Test</title>
+</head>
+<body>
+    <h1>PharmAssist is Working!</h1>
+    <p>This is a minimal test to verify deployment.</p>
+</body>
+</html>
+""")
